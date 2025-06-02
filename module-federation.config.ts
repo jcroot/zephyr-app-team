9ad22d8a75395a79811f949083f2c0d6dac13b54
Jean Claude Adams <tech@arrobasoft.net>
2025-06-02 15:43:13 -0300

    Update project

--- module-federation.config.ts
+++
@@ -1,9 +0,0 @@
-export const mfConfig = {
-  name: "app_team",
-  filename: "remoteEntry.js",
-  exposes: {
-    "./TeamView": "./src/pages/TeamView",
-    "./TeamEdit": "./src/components/TeamEdit",
-  },
-  shared: ["react", "react-dom", "react-router-dom"],
-};
